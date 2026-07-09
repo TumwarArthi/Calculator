b09409537505dd3a8ac144c434043694be155a28
anjalipenke2427 <anjalipenke2427@gmail.com>
2026-07-09 11:42:56 +0530

    feature1 Added

---
+++ src/main/java/com/alpha/calcutor/CalculatoreImplementation.java
@@ -0,0 +1,10 @@
+package com.alpha.calcutor;
+
+public class CalculatoreImplementation implements Calculator {
+
+	@Override
+	public void add() {
+		System.out.println("THIS IS THE ADDING METHOD IMPLEMENTATION");
+	}
+
+}
