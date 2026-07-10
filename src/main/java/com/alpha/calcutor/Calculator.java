09b61e23f29032a0557ad6bf379f67c0cd65c996
TumwarArthi <tumwararthi72@gmail.com>
2026-07-10 10:48:55 +0530

    first commit

--- src/main/java/com/alpha/calcutor/Calculator.java
+++ src/main/java/com/alpha/calcutor/Calculator.java
@@ -2,4 +2,5 @@ package com.alpha.calcutor;
 
 public interface Calculator {
 	public void add();
+	public void sub();
 }
---
+++ src/main/java/com/alpha/calcutor/Demo.java
@@ -0,0 +1,7 @@
+package com.alpha.calcutor;
+
+public class Demo {
+	 public static void main(String[] args) {
+		System.out.println("this is demo class");
+	}
+}
